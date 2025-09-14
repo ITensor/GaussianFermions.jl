d6eaf60c16275662020db38a7d7a1580ab3a7596
Miles <miles.stoudenmire@gmail.com>
2025-09-14 19:39:54 +0200

    Minor change

--- src/operations.jl
+++ src/operations.jl
@@ -1,9 +1,10 @@
 import LinearAlgebra as la
 
-function expect(G::GaussianOperator, ϕ::GaussianState)
-  return la.tr(matrix_elements(G)*correlation_matrix(ϕ))
+function expect(G::GaussianOperator, ψ::GaussianState)
+  return la.tr(matrix_elements(G)*correlation_matrix(ψ))
 end
 
-function expect(G::SpinGaussianOperator, ϕ::SpinGaussianState)
-  return expect(up_operator(G), up_state(ϕ)) + expect(dn_operator(G), dn_state(ϕ))
+function expect(G::SpinGaussianOperator, ψ::SpinGaussianState)
+  return expect(up_operator(G), up_state(ψ)) + expect(dn_operator(G), dn_state(ψ))
+end
 end
