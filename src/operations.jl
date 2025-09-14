a3b5663ab3678947cad891cf0d5ae7bb2285f5c0
Miles <miles.stoudenmire@gmail.com>
2025-09-14 19:40:24 +0200

    Define greens_function and time_evolve. Still need to test these.

--- src/operations.jl
+++ src/operations.jl
@@ -7,4 +7,15 @@ end
 function expect(G::SpinGaussianOperator, ψ::SpinGaussianState)
   return expect(up_operator(G), up_state(ψ)) + expect(dn_operator(G), dn_state(ψ))
 end
+
+function greens_function(H::GaussianOperator, t::Number)
+  ϵ, ϕ = energies_states(G)
+  exp_itϵ = [exp(-im*t*ϵ[n]) for n=1:length(ϵ)]
+  return ϕ*la.Diagonal(exp_itϵ)*ϕ'
+end
+
+function time_evolve(H::GaussianOperator, ψ::GaussianState, t::Number)
+  Gt = greens_function(H,t)
+  orbs_t = Gt*orbitals(ψ)
+  return GaussianState(orbs_t, filling(ψ))
 end
