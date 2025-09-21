42032fc6d9451d51c7307c39056976a8a1d375f0
Miles <miles.stoudenmire@gmail.com>
2025-09-21 16:19:11 -0400

    Update examples

--- examples/time_evolution.jl
+++ examples/time_evolution.jl
@@ -1,8 +1,10 @@
 import GaussianFermions as gf
 
+include("utilities/write_data.jl")
+
 let
-  N = 20
-  Nf = N÷2
+  N = 7
+  Nf = (N-1)÷2
 
   H = gf.GaussianOperator(N)
   for j in 2:(N - 1)
@@ -12,27 +14,36 @@ let
   B = gf.GaussianOperator(N)
   B = gf.add_hop(H, 1, 2, -1)
 
-  display(gf.matrix_elements(H))
-
-  E0, ϕ0 = gf.ground_state(H; Nf)
+  E0, ψ0 = gf.ground_state(H; Nf)
   @show E0
-  @show gf.expect(H, ϕ0)
-  @show gf.entanglement(ϕ0, 1:5)
-  @show gf.bond_dimension(ϕ0, 1:5, 1E-7)
+  @show gf.expect(H, ψ0)
+  @show gf.entanglement(ψ0, 1:N÷2)
+  @show gf.bond_dimension(ψ0, 1:N÷2, 1E-7)
 
   A = gf.GaussianOperator(N)
   A = gf.add_cdag_c(A, 1, 2)
 
+  t0 = 2.0
+  σ = 0.20
+  coef = 0.1
+  field(t) = coef*exp(-(t-t0)^2/(2σ^2))/√(2π*σ^2)
+
   dt = 0.02
-  T = 5.0
+  T = 30.0
   time_range = 0:dt:T
   At = copy(A)
-  Avals = zeros(length(time_range))
+  Avals = zeros(ComplexF64,length(time_range))
   for (n, t) in enumerate(time_range)
-    Ht = H + f(t)*B
-    At = time_evolve(Ht, dt, At)
-    Avals[n] = gf.expect(H, ϕ0)
+    Ht = H + field(t)*B
+    At = gf.time_evolve(Ht, dt, At)
+    Avals[n] = gf.expect(At, ψ0)
   end
+
+  times = collect(time_range)
+
+  write_data("A_real.dat",times,real(Avals))
+  write_data("A_imag.dat",times,imag(Avals))
+  write_data("field.dat",times,field.(times))
 
   return nothing
 end
---
+++ examples/utilities/write_data.jl
@@ -0,0 +1,15 @@
+using Printf: @sprintf
+
+format_string(i::Integer) = @sprintf("%d", i)
+format_string(x::Float64) = @sprintf("%.14f", x)
+function format_string(x)
+  error("Writing to data file of type $(typeof(x)) not currently supported")
+end
+
+function write_data(fname::AbstractString, xv::Vector, yv::Vector)
+  f = open(fname, "w")
+  for (x, y) in zip(xv, yv)
+    write(f, "$(format_string(x)) $(format_string(y))\n")
+  end
+  close(f)
+end
